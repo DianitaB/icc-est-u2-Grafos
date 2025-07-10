3f3e2798b0054c04867b48d9a756776f40e52d63
Diana Borja <valeriaborja975@gmail.com>
2025-07-10 08:28:32 -0500

    Grafos

---
+++ src/models/Node.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Node {
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    
+
+    
+}
